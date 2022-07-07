89b594f38065fb604af903881774435c416ece66
emmamarks27 <100089874+emmamarks27@users.noreply.github.com>
2022-07-07 20:11:57 +0100

    Added Increment/Decrement Functions and storing baskest as an object.

--- src/java.js
+++ src/java.js
@@ -1,10 +1,108 @@
-let fruits = [
-  { fruit: 'oranges', quantity: 3 },
-  { fruit: 'kiwis', quantity: 12 },
-  { fruit: 'bananas', quantity: 5 },
-  { fruit: 'lemons', quantity: 7 },
-  { fruit: 'limes', quantity: 3 },
-];
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready);
+} else {
+  ready();
+}
+
+function ready() {
+  let removeFromCart = document.getElementsByClassName('remove-from-cart');
+  for (var i = 0; i < removeFromCart.length; i++) {
+    var button = removeFromCart[i];
+    button.addEventListener('click', removeCartItem);
+  }
+
+  var addToCartButtons = document.getElementsByClassName('add-to-cart');
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    button.addEventListener('click', addToCartClicked);
+  }
+}
+
+function addToCartClicked(event) {
+  var button = event.target;
+  var shopItem = button.parentElement;
+  var title = shopItem.querySelector('.shop-item-title').innerText;
+  //var imgSrc = shopItem.getElementsByClassName('fruit-img')[0].src;
+  var basketOuterStructure = {};
+  var basketInnerStructure = {
+    quantity: 0,
+  };
+  basketInnerStructure.quantity = 1;
+  basketOuterStructure[title] = basketInnerStructure;
+  basketObject = Object.assign(userBasket, basketOuterStructure);
+  addItemToCart(title);
+  updateCartTotal();
+  return basketObject;
+}
+
+function addItemToCart(title) {
+  var cartRow = document.createElement('div');
+  cartRow.innerText = title;
+  var cartItems = document.getElementsByClassName('cart-wrapper')[0];
+  var cartItemsNames = document.getElementsByClassName('cart-item');
+
+  for (var i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerText == title) {
+      alert('This item is already added to the cart.');
+      return;
+    }
+  }
+
+  var cartRowContents = `<div class="cart-row">
+                          <span class="cart-item cart-column">${title}</span>
+                          <i class="fa-solid fa-minus cart-decrease"></i>
+                          <span class="cart-quantity cart-column">1</span>
+                          <i class="fa-solid fa-plus cart-increase"></i>
+                          <button class="remove-from-cart cart-column">
+                              Remove from Cart
+                            </button>
+                        </div>`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow
+    .getElementsByClassName('remove-from-cart')[0]
+    .addEventListener('click', removeCartItem);
+  cartRow
+    .getElementsByClassName('cart-increase')[0]
+    .addEventListener('click', cartIncrease);
+  cartRow
+    .getElementsByClassName('cart-decrease')[0]
+    .addEventListener('click', cartDecrease);
+}
+
+function cartIncrease(event) {
+  var cartItemClicked = event.target;
+  var cartItem = cartItemClicked.parentElement;
+  var Quantity = cartItem.querySelector('.cart-quantity');
+  var cartQuantity = parseInt(Quantity.innerText);
+  var totalQuantity = cartQuantity + 1;
+  Quantity.innerText = totalQuantity;
+  var title = cartItem.querySelector('.cart-item').innerText;
+  basketObject[title].quantity += 1;
+  updateCartTotal();
+}
+
+function cartDecrease(event) {
+  var cartItemClicked = event.target;
+  var cartItem = cartItemClicked.parentElement;
+  var Quantity = cartItem.querySelector('.cart-quantity');
+  var cartQuantity = parseInt(Quantity.innerText);
+  if (cartQuantity > 1) {
+    var totalQuantity = cartQuantity - 1;
+    Quantity.innerText = totalQuantity;
+    var title = cartItem.querySelector('.cart-item').innerText;
+    basketObject[title].quantity -= 1;
+    updateCartTotal();
+  } else {
+    alert('Please delete your item from your shopping cart.');
+  }
+}
+
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateCartTotal();
+}
 
 function isLoaded() {
   isFullyLoaded = true;
@@ -14,22 +112,15 @@ function isLoaded() {
 function displayFruits(fruit) {
   testFruitsContent =
     testFruitsContent +
-    `<div class="fruit-section" id="id-${fruit.fruit}><h2 id="fruit-title">${fruit.fruit}</h2>
-          <img src="src/images/${fruit.fruit}.jpeg" class="fruit-img"  alt="photo of ${fruit.fruit}" ></img>
-          <div id="fruit-quantity">${fruit.quantity}</div>  
-          <button value=${fruit.fruit} onclick="handleAddToCart(this.value)" class="add-to-cart">Add to cart</button></div>`;
+    `<div class="fruit-section" id="id-${fruit.fruit}>
+          <span class="shop-item-title"> 
+            <h2 class="shop-item-title">${fruit.fruit}</h2>
+          </span>      
+            <img src="src/images/${fruit.fruit}.jpeg" class="fruit-img"  alt="photo of ${fruit.fruit}" ></img>  
+          <button value=${fruit.fruit} class="add-to-cart">Add to cart</button>
+    </div>`;
   testFruits.innerHTML = testFruitsContent;
   isLoaded();
-}
-
-let removeFromCart = document.getElementsByClassName('remove-from-cart');
-for (var i = 0; i < removeFromCart.length; i++) {
-  var button = removeFromCart[i];
-  button.addEventListener('click', function (event) {
-    var buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
-    updateCartTotal();
-  });
 }
 
 function updateCartTotal() {
@@ -38,21 +129,27 @@ function updateCartTotal() {
   var total = 0;
   for (var i = 0; i < cartRows.length; i++) {
     var cartRow = cartRows[i];
-    var quantity = cartRow.getElementsByClassName('cart-quantity')[0];
-    var quantityElement = parseFloat(quantity.innerText);
-    console.log(quantityElement);
+    var quantity = cartRow.querySelector('.cart-quantity');
+    var quantityElement = parseInt(quantity.innerText);
     total = total + quantityElement;
   }
   var cartTotal = document.getElementById('cart-total');
   cartTotal.innerHTML = `${total}`;
 }
 
+let fruits = [
+  { fruit: 'oranges', quantity: 3 },
+  { fruit: 'kiwis', quantity: 12 },
+  { fruit: 'bananas', quantity: 5 },
+  { fruit: 'lemons', quantity: 7 },
+  { fruit: 'limes', quantity: 3 },
+];
+
 let isFullyLoaded = false;
-let updateBasket = ' ';
 let testFruitsContent = ' ';
 let testFruits = document.querySelector('.test-fruits');
 fruits.forEach(displayFruits);
-
-let fruitButton = document.querySelector('.add-to-cart');
-//fruitButton.addEventListener("click", handleAddToCart);
-let basket = document.getElementById('basket');
+let userBasket = {
+  fruit: '',
+  quantity: 0,
+};
